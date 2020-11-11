3399249235bf2d36b30c3b71b16130c152438165
josegiron765 <dragonike765@gmail.com>
2020-11-11 00:15:12 -0500

    practica 10

---
+++ segundo corte/practica 10/ejercicio1/ejercicio1/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Ejercicio_1
+{
+    //Como ejercicio de herencia vamos a realizar una suma y resta 
+    class Program
+    {
+        public class Operacion
+        {
+            protected int F1;
+            protected int F2;
+            protected int Rsl;
+            //Primeor vamos a declarar las variables que necesitamos para la ejecucion de nuestra herencia
+            public int Fa1
+            {
+                set
+                {
+                    F1 = value;
+                }
+                get
+                {
+                    return F1;
+                }
+            }
+
+            public int Fa2
+            {
+                set
+                {
+                    F2 = value;
+                }
+                get
+                {
+                    return F2;
+                }
+            }
+            //Declaramos los primeros valores los cuales van a recibir la informacion que le vamos a ingresar y nos la van a devolver con el return 
+            public int R
+            {
+                protected set
+                {
+                    Rsl = value;
+                }
+                get
+                {
+                    return Rsl;
+                }
+            }
+            //aqui mostraremos los resultados, igual que en el aterior le damos la orden que nos muestre el ultimo valor guardado con el return
+        }
+        public class Suma : Operacion
+        {
+            public void Operar()
+            {
+                R = Fa1 + Fa2;
+            }
+        }
+        public class Resta : Operacion
+        {
+            public void Operar()
+            {
+                R = Fa1 - Fa2;
+            }
+        }
+        //para que sea una herencia necesitaremos mas de un class y en ellos les daremos las diferentes ordenes con las cuales queremos ejecutar el codigo
+        //estors los entrelasamos con el class principal que tenemos, para asi poder usar los valores asignasdon en el
+        class Prueba
+        {
+            static void Main(string[] args)
+            {
+                Suma S = new Suma();
+                S.Fa1 = 520;
+                S.Fa2 = 680;
+                S.Operar();
+                Console.WriteLine("La suma de " + S.Fa1 + " + " + S.Fa2 + " = " + S.R);
+                Resta Rs = new Resta();
+                Rs.Fa1 = 1349;
+                Rs.Fa2 = 253;
+                Rs.Operar();
+                Console.WriteLine("La resta de " + Rs.Fa1 + " - " + Rs.Fa2 + " = " + Rs.R);
+                Console.ReadKey();
+            }
+        }
+        //Por ultimo en eeste lamaremos a las clases que nos interesan las cuales van a ser las que realizen la suma y resta de nuestro algoridmo
+        //ya que todas las clases estan conectadas poderemos llamar a los valores que usamos dentro de ella para poder usarlo como se muestra anteriormente
+        //ingrecamos todo en el void principal para mostrar por pantalla el ejercicio y comprobando que la herencia este bien hecha
+    }
+}
---
+++ segundo corte/practica 10/ejercicio2/ejercicio2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+// como ejercicio de herencia vamos a relaizar la impresion de dos frases 
+// Primero creamos una clase A junto con las variables que nesecitamos la la ejecucion de la herencia 
+public class A
+{
+    private string f1 =" ..Aquellos que no son capacez de reconocerce a si mismos estan destinados a fracasar.. ";
+    private string f2 = "..Perdon por el retraso me perdi por el sendero de la vida.. ";
+    // Definimos otra clase y devolvemos con el metodo return la informacion ya suministrada
+    public class B : A
+    {
+        public string f()
+        {
+            return this.f1;
+        }
+    }
+    // asi mismo creamos otra clase para la otra frase devolviendola igualmente con el metodo return
+
+    public class C : A
+    {
+        public string fr2()
+        {
+            return this.f2;
+        }
+    }
+
+
+}
+
+
+// para que sea una herencia nesecitamos mas una clase como las que ya tenemos y las unimos todas en nuestra clase principal en donde imprimimos las dos frases 
+public class Example
+{
+    public static void Main(string[] args)
+    {
+        var b = new A.B();
+        var c = new A.C();
+        Console.WriteLine(" FRASE NUMERO 1 :");
+        Console.WriteLine(b.f());
+        Console.WriteLine(" FRASE NUMERO 2 :");
+        Console.WriteLine(c.fr2());
+        Console.ReadKey();
+    }
+}
